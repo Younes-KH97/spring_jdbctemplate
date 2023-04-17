b663d63bf259c56239f901577233edb52d98016e
Younes.khe97 <younes.khellaf97@gmail.com>
2023-04-17 02:06:53 +0100

    add ingredient repository

---
+++ src/main/java/cloud/data/IngredientRepository.java
@@ -0,0 +1,13 @@
+package cloud.data;
+
+import cloud.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
